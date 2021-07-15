8f4ab7a3ef0b68dbfb8cb981acf8c1c79a58a7a6
Jen Buxton <jen.buxton@gmail.com>
2021-07-15 15:58:45 +1200

    Add db, toggle home

--- client/components/BirdsGallery.jsx
+++ client/components/BirdsGallery.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import { getBirds } from '../apis/birds'
+import { activePage as setActivePage } from '../actions/activePage'
 
-function BirdsGallery(props) {
-    return (
-        <h1>Birbs</h1>
-    )
+function BirdsGallery (props) {
+  const { dispatch } = props
+  const [birds, setBirds] = useState([])
+
+  useEffect(() => {
+    return getBirds()
+      .then(res => {
+        console.log('component res', res)
+        setBirds(res)
+        return null
+      })
+      .catch(e => console.log(e))
+  }, [])
+
+  function handleClick () {
+    dispatch(setActivePage('Homepage'))
+  }
+
+  return (
+    <>
+      <h1>Birds Gallery</h1>
+      <ul>
+        {birds.map((bird, i) => <li key={i}>{bird.commonName}</li>)}
+      </ul>
+      <button onClick={handleClick}>Back to Recently Observed</button>
+    </>
+  )
 }
 
 export default connect()(BirdsGallery)
