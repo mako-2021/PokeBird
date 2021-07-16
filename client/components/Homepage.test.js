234673c07e2945007c9eb25cb43fe41bd1921aed
pavarisk <p.kakanat@gmail.com>
2021-07-16 16:55:53 +1200

    Homepage test fn in prog

--- client/components/Homepage.test.js
+++ client/components/Homepage.test.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { screen, render } from '@testing-library/react'
 
 import Homepage from './Homepage'
-import store from '../store'
+// import store from '../store'
 import { fetchRecentObs } from '../actions/observed'
 
 const fakeStore = {
@@ -16,22 +16,36 @@ jest.mock('../actions/observed', () => ({
   fetchRecentObs: jest.fn
 }))
 
-fakeStore.getState.mockImplementation(() => {
-  {speciesCode:'easros1',comName…91802490'},
-  {speciesCode:'dunnoc1',comName…91802490'},
-  {speciesCode:'comcha',comName:…91802490'},
-  {speciesCode:'sonthr1',comName…91802490'},
-  {speciesCode:'houspa',comName:…91802490'}
+fakeStore.getState.mockImplementation(() => ({
+  observed: [
+    { speciesCode: 'easros1', comName: 'Eastern Rosella' },
+    { speciesCode: 'tui123', comName: 'tui' },
+    { speciesCode: 'kf123', comName: 'kingfisher' },
+    { speciesCode: 'dunnoc1', comName: 'Dunnock' }
+  ]
+}))
+
+describe('<Homepage />', () => {
+  test('list out observed from redux', async () => {
+    render(<Provider store={fakeStore}><Homepage /></Provider>)
+    const observed = await screen.getByTestId('bird')
+    expect(observed).toHaveLength(4)
+  })
+  // test('loads todos from api on intial mount', () => {
+  //   render(<Provider store={fakeStore}><Todos /></Provider>)
+  //   expect(fakeStore.dispatch).toHaveBeenCalled()
+  //   expect(fetchTodos).toHaveBeenCalled()
+  // })
 })
 
 // fetchRecentObs.mockImplementation(() => () => {})
 
-test('testing header is Map', () => {
-  render(<Provider store={store}><Homepage /></Provider>)
-  const heading = screen.getByRole('heading')
-  expect(heading.innerHTML).toMatch('Map')
-})
+// test('testing header is Map', () => {
+//   render(<Provider store={store}><Homepage /></Provider>)
+//   const heading = screen.getByRole('heading')
+//   expect(heading.innerHTML).toMatch('Map')
+// })
 
-test('observed.length === markers on map', () => {
+// test('observed.length === markers on map', () => {
 
-})
+// })
