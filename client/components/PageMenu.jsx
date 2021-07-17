4c1b19ab6930c3098953d147f918fca82a09f629
pavarisk <p.kakanat@gmail.com>
2021-07-17 16:47:20 +1200

    added user button when logged in

--- client/components/PageMenu.jsx
+++ client/components/PageMenu.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { setActivePage } from '../actions/activePage'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export const PageMenu = (props) => {
   const { dispatch } = props
+  const { isAuthenticated } = useAuth0()
+
 
   function handleClick(e) {
     e.preventDefault()
@@ -15,6 +18,9 @@ export const PageMenu = (props) => {
     <div>
       <button onClick={e => handleClick(e)} value={'Homepage'}>Homepage</button>
       <button onClick={e => handleClick(e)} value={'Gallery'}>Gallery</button>
+      {isAuthenticated && 
+      <button onClick={e => handleClick(e)} value={'User'}>User</button>
+      }
     </div>
   )
 }
