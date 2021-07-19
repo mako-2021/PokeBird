fee9528bdc6b3e92cb37ac2f26a5e982242f0383
marina <golovina.marina@gmail.com>
2021-07-20 09:12:14 +1000

    Add more questions to the quiz component

--- client/apis/quizData.js
+++ client/apis/quizData.js
@@ -7,6 +7,7 @@ const quizData = [
             { answerText: 'around 50%', isCorrect: false },
             { answerText: 'at least 85%', isCorrect: true }
         ],
+        correctAnswer: 'at least 85%',
         imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/765be0cc148c4606be5d1e51bdcfb973/2013-albatross-chick-1920-2.jpg'
     },
     {
@@ -16,6 +17,7 @@ const quizData = [
             { answerText: 'Yes', isCorrect: true },
             { answerText: 'No', isCorrect: false }
         ],
+        correctAnswer: 'Yes',
         imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/a9df6771b0394be39a12138a802ae8fa/bellbird-s-evans-1920-2.jpg'
     },
     {
@@ -28,7 +30,83 @@ const quizData = [
         ],
         correctAnswer: 'Chatham Islands black robin',
         imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/e2d052e58a524678834413ef47993150/chathams-island-black-robin-hero.jpg'
-    }
+    },
+    {
+        id: 4,
+        questionText: 'What is the Māori name for Silvereye bird?',
+        answerOptions: [
+            { answerText: 'Tauhou', isCorrect: true },
+            { answerText: 'Mātātā', isCorrect: false },
+            { answerText: 'Ruru', isCorrect: false }
+        ],
+        correctAnswer: 'Tauhou',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/cec52a74b8b44a65bb3e703f44b5bec9/silvereye-jj-harrison-1920.jpg'
+    },
+    {
+        id: 5,
+        questionText: 'Which bird has a distinctive noisy beat of its wings?',
+        answerOptions: [
+            { answerText: 'Pūkeko', isCorrect: true },
+            { answerText: 'Kererū', isCorrect: false },
+            { answerText: 'Kiwi', isCorrect: false }
+        ],
+        correctAnswer: 'Kererū',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/d858de7f16ff4eef8c4276fdeb37501b/kereru-taieri-mouth-1920-2.jpg'
+    },
+    {
+        id: 6,
+        questionText: 'How many shelduck species are present in NZ?',
+        answerOptions: [
+            { answerText: 'more than 10', isCorrect: false },
+            { answerText: '1', isCorrect: true },
+            { answerText: '3', isCorrect: false }
+        ],
+        correctAnswer: '1 - The paradise shelduck is New Zealand’s only shelduck',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/34b7142a580b47518ab7923755211ae0/paradise-duck-female-1920.jpg'
+    },
+    {
+        id: 7,
+        questionText: 'What is one of New Zealand’s rarest birds?',
+        answerOptions: [
+            { answerText: 'Stitchbird/hihi', isCorrect: true },
+            { answerText: 'Wrybill/ngutu pare', isCorrect: false },
+            { answerText: 'Yellowhead/mohua', isCorrect: false }
+        ],
+        correctAnswer: 'Stitchbird (Māori name: hihi)',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/6c492a45411948dfa0538357e9188100/stitchbird-hihi-michael-hamilton-1920-2.jpg'
+    },
+    {
+        id: 8,
+        questionText: 'What is one of New Zealand’s rarest birds?',
+        answerOptions: [
+            { answerText: 'Stitchbird/hihi', isCorrect: true },
+            { answerText: 'Wrybill/ngutu pare', isCorrect: false },
+            { answerText: 'Yellowhead/mohua', isCorrect: false }
+        ],
+        correctAnswer: 'Stitchbird (Māori name: hihi)',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/6c492a45411948dfa0538357e9188100/stitchbird-hihi-michael-hamilton-1920-2.jpg'
+    },
+    {
+        id: 9,
+        questionText: 'Which one of the below New Zealand birds can fly?',
+        answerOptions: [
+            { answerText: 'Weka', isCorrect: false },
+            { answerText: 'Takahē', isCorrect: false },
+            { answerText: 'New Zealand dotterel', isCorrect: true }
+        ],
+        correctAnswer: 'New Zealand dotterel',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/568d611f8c544503b37f0f9dca26bfa9/southern-nz-dotterel-1920-2.jpg'
+    },
+    {
+        id: 10,
+        questionText: 'Keas are considered to be one of the most intelligent birds in the world. Is this true or false?',
+        answerOptions: [
+            { answerText: 'True', isCorrect: true },
+            { answerText: 'False', isCorrect: false }
+        ],
+        correctAnswer: 'True',
+        imageURL: 'https://www.doc.govt.nz/thumbs/hero/contentassets/16e1b2847aaa47789d5f7ffaf37b0c7b/female-kea-feeding-chick-fiordland-1920.jpg'
+    },
 ];
 
 export default quizData
--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -26,22 +26,29 @@ function Quiz() {
     }
 
     return (
-        <>
+        <div className='quiz-wrapper'>
             {load ?
                 <div className='row'>
                     {showScore ? (
-                        <div className='column'>
-                            <div className='card'>
-                                You scored {score} out of {quizData.length}
+                        <div className='column' style={{ width: '600px' }}>
+                            <div className='card' style={{ width: '400px' }}>
+                                <h2>You scored {score} out of {quizData.length}</h2>
+                                <div>Correct answers:
+                                    <ul className='quiz-list'>
+                                        {quizData.map((data, i) => <li className='quiz-correct' key={i}>Question {data.id}: {data.questionText} <br /> {data.correctAnswer} <br /> <img src={data.imageURL} style={{ maxWidth: '200px', borderRadius: '10px' }}></img></li>)}
+                                    </ul>
+                                    Find more on <a href='https://www.doc.govt.nz/nature/native-animals/birds/birds-a-z/'>DOC Website</a><br />
+                                    This quiz has been made using the information provided on the DOC website (including all photography works). Attribution 4.0 International (CC BY 4.0) <a href='https://creativecommons.org/licenses/by/4.0/legalcode'>Legal Code</a>
+                                </div>
                             </div>
                         </div>
 
                     ) : (
-                        <div className='column'>
-                            <div className='card'>
-                                Question {currentQuestion + 1}/{quizData.length} <br />
+                        <div className='column' style={{ width: '600px' }}>
+                            <div className='card' style={{ width: '400px' }} >
+                                Question {currentQuestion + 1} /{quizData.length} <br />
                                 {quizData[currentQuestion].questionText}
-                                <ul>{quizData[currentQuestion].answerOptions.map((answerOption, i) => (<li key={i} > <button onClick={() => handleAnswer(answerOption.isCorrect)}>{answerOption.answerText}</button></li>
+                                <ul className='quiz-answers'>{quizData[currentQuestion].answerOptions.map((answerOption, i) => (<li key={i} > <button className='quiz-button' onClick={() => handleAnswer(answerOption.isCorrect)}>{answerOption.answerText}</button></li>
                                 ))}
                                 </ul>
                             </div>
@@ -53,13 +60,13 @@ function Quiz() {
                     <div className='column'>
                         <div className='card'>
                             <h2 id="quiz">Ngā manu – Birds</h2>
-                            <button onClick={handleClick}>Start a quiz</button>
-                            <img src='https://www.doc.govt.nz/thumbs/hero/contentassets/59ca125424394c748fb9988e93411190/brown-kiwi-hero.jpg' style={{ maxWidth: '400px', borderRadius: '10px' }}></img>
+                            <button onClick={handleClick}>Start the quiz</button>
+                            <img src='images/placeholder-pidgey.png' style={{ height: '200px', width: '200px', borderRadius: '50%', background: 'white' }}></img>
                         </div>
                     </div>
                 </div>
             }
-        </>
+        </div >
     )
 
 }
