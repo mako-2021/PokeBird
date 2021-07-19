657f9ac71df427fe469241c1dbba37cb8b9907ce
pavarisk <p.kakanat@gmail.com>
2021-07-19 16:17:31 +1200

    add/remove function working again

--- server/db/usersObs.js
+++ server/db/usersObs.js
@@ -4,13 +4,13 @@ const getUserObsBySub = (userSub, db = connection) => {
   return db('usersObs')
     .join('birds', 'birds.id', 'usersObs.birds_id')
     .where('user_sub', userSub)
-    .select()
+    .select('usersObs.*', 'birds.commonName')
 }
 
 const addUserObs = (observation, db = connection) => {
   const obs = {
     user_sub: observation.userSub,
-    birds_id: observation.bird.id
+    birds_id: observation.id
   }
   return db('usersObs').insert(obs)
 }
