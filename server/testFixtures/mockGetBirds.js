e7548e2cb806bd945079243a9b1f6cd8ccaaef1b
Henry Glenn <henryglenn43@gmail.com>
2021-07-20 12:27:25 +1200

    routes test done

---
+++ server/testFixtures/mockGetBirds.js
@@ -0,0 +1,90 @@
+const mockGetBirds = {
+  result: [{
+    id: 35,
+    commonName: 'White-capped albatross',
+    latinName: 'Thalassarche cauta',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Native',
+    conservationStatus: 'Declining',
+    length: '90 - 100 cm',
+    weight: '3.4 - 4.4 kg',
+    description: 'A medium-sized albatross with white head, neck, lower back and rump, black across the upperwings, black-tipped white tail, a small dark patch in front of each eye and light grey cheeks. The blue-grey bill has a yellowish tip and the underparts are white with narrow black borders under the wing and a small black notch in the armpit at the base of the leading edge of the wing.',
+    image: '/images/035.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/white-capped-mollymawk'
+  },
+  {
+    id: 36,
+    commonName: 'Bullers albatross',
+    latinName: 'Thalassarche bulleri',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Endemic',
+    conservationStatus: 'Naturally Uncommon',
+    length: '80 cm',
+    weight: '2.5 - 3.5 kg',
+    description: 'A small albatross with light-grey neck and throat, silvery-grey crown, black across the upperwings, white lower back and rump, black-tipped tail, and a black bill with yellow top and bottom plates. The underparts are white with a clear-cut broad black leading edge and narrow black trailing edge under the wing.',
+    image: '/images/036.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/bullers-mollymawk'
+  },
+  {
+    id: 37,
+    commonName: 'Light-mantled albatross',
+    latinName: 'Phoebetria palpebrata',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Native',
+    conservationStatus: 'Declining',
+    length: '80 - 90 cm',
+    weight: '2.8 - 3.1 kg',
+    description: 'A small dark brown albatross with a pale greyish-brown mantle and back, extremely pointed wings, long pointed tail, a white crescent around the eye, and a pale blue stripe along the lower mandible in contrast with the otherwise black bill and dark-brown head. Their flight is effortless, and courting birds at breeding grounds are often seen in pairs in a synchronised aerodynamic display.',
+    image: '/images/037.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/light-mantled-sooty-albatross'
+  },
+  {
+    id: 38,
+    commonName: 'Northern giant petrel',
+    latinName: 'Macronectes halli',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Native',
+    conservationStatus: 'Recovering',
+    length: '80 - 95 cm',
+    weight: '3 - 5 kg',
+    description: 'A large petrel with a dark grey-brown body, pale grey face, pale whitish eyes, pinkish-yellow horn bill with red-brown tip, and dark grey legs. Juveniles are dark sooty black with a yellowish bill and become paler-headed with age.',
+    image: '/images/038.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/northern-giant-petrel'
+  },
+  {
+    id: 39,
+    commonName: 'Cape petrel',
+    latinName: 'Daption capense',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Native',
+    conservationStatus: 'Naturally Uncommon',
+    length: '40 cm',
+    weight: '450 g',
+    description: 'A medium-sized black-and-white petrel with a black head, neck and mantle, a white lower back, base of the upperwing and rump heavily chequered with black, and black bill, legs and feet. The outer upperwings are black with broad white patches near the body and beyond the bend in the wing; the underparts and underwings are white with a black leading edge to the wings.',
+    image: '/images/039.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/cape-petrel'
+  },
+  {
+    id: 40,
+    commonName: 'Grey-faced petrel',
+    latinName: 'Pterodroma macroptera',
+    birdOrder: 'Procellariiformes',
+    nzStatus: 'Native',
+    conservationStatus: 'Not Threatened',
+    length: '42 cm',
+    weight: '550 g',
+    description: 'A large dark gadfly petrel with uniformly dark black-brown body with occasional paler brown worn feathers, long narrow wings, a long pointed tail, and black eyes and legs. The stout black bill has a large sharp hook and the base of the bill and throat are either grey or buff-white.',
+    image: '/images/040.png',
+    audio: '#',
+    url: 'http://nzbirdsonline.org.nz/species/grey-faced-petrel'
+  }]
+}
+
+module.exports = {
+  mockGetBirds
+}
