66a17a1a11aa1a0a6f36d9242e4afe03e4dbd233
marina <golovina.marina@gmail.com>
2021-07-20 21:54:38 +1000

    Add consistent CSS styling to the tooltip element on the map

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -20,45 +20,59 @@ function Homepage(props) {
     if (latitude !== undefined && longitude !== undefined) {
       return fetchRecentObs()
         .then(observations => {
+          console.log(observations)
           dispatch(setRecentObs(observations))
           return null
         })
     }
   }, [latitude, longitude])
 
+  if (latitude == undefined && longitude == undefined) {
+    return (
+      <div className='loader'>
+        <h2> <i class="fas fa-kiwi-bird"></i> Map is loading... <i class="fas fa-kiwi-bird"></i> </h2>
+        {/* <BounceLoader color={'blue'} size={60} /> */}
+      </div>
+    )
+  }
+
   return (
     <>
-      <h1 style={{ textAlign: 'center' }}>Reports of the recent bird viewings in New Zealand. Source: eBird </h1>
-      <div id="mapid" style={{ display: 'block', margin: '0 auto', height: '600px', width: '600px' }}>
-        <MapContainer center={[-40.90, 174.77]} zoom={6} scrollWheelZoom={true} style={{ height: '600px', width: '600px' }}>
+      <h3 style={{ textAlign: 'center' }}> <i class="fas fa-search-location"></i> Reports of the recent bird viewings in New Zealand. Source: eBird </h3>
+      <div id="map" style={{ display: 'block', margin: '0 auto', height: '500px', width: '500px' }}>
+        <MapContainer center={[-40.90, 174.77]} zoom={5} scrollWheelZoom={true} style={{ height: '500px', width: '500px' }}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker position={[-41.29, 174.77]}>
-            <div id="customTooltip">
-              <Tooltip
-                offset={[0, 20]}
-                direction='bottom'
-              >
-                Wellington
-              </Tooltip>
-            </div>
+
+            <Tooltip
+              offset={[0, 20]}
+              direction='bottom'
+              opacity={1}
+              className='bird-info-wellington'
+              permanent={false}
+            >
+              <div className='bird-info-text-wellington'>Wellington</div>
+            </Tooltip>
+
           </Marker>
 
           {observed.map((observation, i) => <Marker data-testid='bird' icon={icon} key={i} position={[observation.lat, observation.lng]}>
-            <div className="customTooltip">
-              <Tooltip
-                offset={[0, 20]}
-                direction='bottom'
-              >
-                <TooltipImage sciName={observation.sciName} />
-                <br />
-                <h3>{observation.comName}</h3>
-                <p>{observation.sciName}</p>
-                <p> seen at {observation.locName} on {observation.obsDt}</p>
-              </Tooltip>
-            </div>
+
+            <Tooltip
+              offset={[0, 20]}
+              direction='bottom'
+              className='bird-info'
+              opacity={1}
+            >
+              <TooltipImage sciName={observation.sciName} />
+              <br />
+              <h3>{observation.comName}</h3>
+              <p> Location: <br /> {observation.locName} <br /> Date: <br /> {observation.obsDt}</p>
+            </Tooltip>
+
           </Marker>)}
         </MapContainer>
       </div>
